8c947481a09da0db2d3d14b684e7ca4f0129cc6d
Panic <panic.milos99@gmail.com>
2023-04-18 23:50:58 +0200

    test

--- release.config.js
+++ release.config.js
@@ -3,14 +3,7 @@ module.exports = {
 	repositoryUrl: "https://github.com/ftrip-io/playground",
 	plugins: [
 		'@semantic-release/commit-analyzer',
-		'@semantic-release/release-notes-generator',
-    [
-      "@semantic-release/exec",
-      {
-        "prepareCmd": "set-version ${nextRelease.version}",
-        "publishCmd": "publish-package"
-      }
-    ]
+		'@semantic-release/release-notes-generator'
 	]
 }
 	
