f9061184fe5b29e4063b95519534382f644c1e7a
Panic <panic.milos99@gmail.com>
2023-04-18 23:33:48 +0200

    test2

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.export = {
 	branches: "master",
-	repositoryUrl: "https://github.com/ftrip-io/playground.git",
+	repositoryUrl: "https://github.com/ftrip-io/playground",
 	plugins: [
 		'@semantic-release/commit-analyzer',
 		'@semantic-release/release-notes-generator',
