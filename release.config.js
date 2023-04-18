169c43d5e76b6c031aae39386d13dba8da27972f
Panic <panic.milos99@gmail.com>
2023-04-18 20:02:09 +0200

    test: added .git to url

--- release.config.js
+++ release.config.js
@@ -1,10 +1,9 @@
 module.export = {
 	branches: "master",
-	repositoryUrl: "https://github.com/ftrip-io/playground",
+	repositoryUrl: "https://github.com/ftrip-io/playground.git",
 	plugins: [
 		'@semantic-release/commit-analyzer',
 		'@semantic-release/release-notes-generator',
-		'@semantic-release/npm',
 		'@semantic-release/github'
 	]
 }
