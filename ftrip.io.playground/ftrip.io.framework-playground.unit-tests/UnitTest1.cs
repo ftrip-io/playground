d552c34033dfb634bae4819d3e9e63c73882c31f
Panic <panic.milos99@gmail.com>
2023-04-24 17:19:53 +0200

    testing workflow without dockerfile

---
+++ ftrip.io.playground/ftrip.io.framework-playground.unit-tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace ftrip.io.framework_playground.unit_tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(false);
+        }
+    }
+}
