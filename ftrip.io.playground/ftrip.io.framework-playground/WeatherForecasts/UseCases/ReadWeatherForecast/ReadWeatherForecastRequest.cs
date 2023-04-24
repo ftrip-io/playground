a99ba149954e215ee0319c46d02a883acd96888f
Panic <panic.milos99@gmail.com>
2023-04-24 19:22:16 +0200

    testing code coverage

---
+++ ftrip.io.playground/ftrip.io.framework-playground/WeatherForecasts/UseCases/ReadWeatherForecast/ReadWeatherForecastRequest.cs
@@ -0,0 +1,11 @@
+﻿using ftrip.io.framework_playground.WeatherForecasts.Domain;
+using MediatR;
+using System;
+
+namespace ftrip.io.framework_playground.WeatherForecasts.UseCases.ReadWeatherForecast
+{
+    public class ReadWeatherForecastRequest : IRequest<WeatherForecast>
+    {
+        public Guid Id { get; set; }
+    }
+}
---
+++ ftrip.io.playground/ftrip.io.framework-playground/WeatherForecasts/UseCases/ReadWeatherForecast/ReadWeatherForecastRequestHandler.cs
@@ -0,0 +1,25 @@
+﻿using ftrip.io.framework_playground.WeatherForecasts.Domain;
+using MediatR;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ftrip.io.framework_playground.WeatherForecasts.UseCases.ReadWeatherForecast
+{
+    public class ReadWeatherForecastRequestHandler : IRequestHandler<ReadWeatherForecastRequest, WeatherForecast>
+    {
+        public ReadWeatherForecastRequestHandler()
+        {
+        }
+
+        public Task<WeatherForecast> Handle(ReadWeatherForecastRequest request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(new WeatherForecast()
+            {
+                Id = Guid.NewGuid(),
+                Active = true,
+                TemperatureC = 20
+            });
+        }
+    }
+}
